73b798ecadebf133ebe52f867e87b20431e2ec91
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-09-20 13:01:14 +0200

    task 1 for last digit of random number commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.rand.int(-1000, 1000)
+
+last_digit = number % 10
+
+if last_digit > 5:
+    print("Last digit of {0} is {1} and is grater than 5".format(number,last_digit))
+elif last_digit == 0:
+    print("Last digit of {0} is {1}".format(number, last_digit))
+elif last_digit < 6 and not 0:
+    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, last_digit))
