934bf6f69536b88548df9efae8f174c8045deb33
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-09-20 12:42:08 +0200

    conditional statement first task commit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,9 +4,9 @@ import random
 
 number = random.randint(-10, 10)
 
-if intiger_number > 0:
+if number > 0:
     print("is positive")
-elif intiger_number == 0:
+elif number == 0:
     print("is zero")
-elif intiger_number < 0:
+elif number < 0:
     print("is negative")
