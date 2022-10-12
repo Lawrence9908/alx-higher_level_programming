f8277e6273b3f1ad0f0c4e7ca930e0cd9cba5369
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-10-13 00:13:55 +0200

    sqaure class task 1 commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+class Square:
+    """Represent a Sqaure"""
+
+    def __init__(self, size):
+        """Initilize a quare
+
+        Args:
+           size (int): The size of new square 
+        """
+        self.__size = size
