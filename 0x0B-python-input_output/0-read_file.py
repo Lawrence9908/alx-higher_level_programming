14a773f4c34836f60af5335f067a5fcd71548a60
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-10-25 21:55:45 +0200

    python program to read file content into stdout update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@ def read_file(filename=""):
     Python function to read the content of the file into stdout
     """
 
-    with open("content.txt", encoding="utf-8") as my_content:
+    with open(filename, encoding="utf-8") as my_content:
         #saving the read line into read_data variable
         read_data = my_content.read()
         #printing out the read line into stdout
