0e6bb5f8c96379fc75f8a8b7b604a9a100ac9bf6
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-11-29 22:37:40 +0200

    list all the records where name is not null

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all records of second table where name is not null.
-SELECT `score`, `name` FROM `second_table` WHERE `name` NOT NULL ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
