e608cd8a0097151247b43116746711d04d7d0519
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-11-29 21:55:47 +0200

    script to create second table and 4 insert records

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,6 @@
 -- Create second table and add multiple records.
-CREATE TABLE IF NOT EXISTS `second_table`(
-	'id' INT,
-	`name` VARCHAR(256),
-	`score` INT
-	);
--- Inserting bulk record into second_table.
-INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(1, "John", 10);
-INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(2, "Alex", 3);
-INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(3, "Bob", 14);
-INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(4, "George", 8);
+CREATE TABLE IF NOT EXISTS `second_table`('id' INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(4, "George", 8);
