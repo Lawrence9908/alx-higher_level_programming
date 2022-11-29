2da2f0b457e82e6561c6b97581b7cfd233c1e38c
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-11-29 22:07:09 +0200

    retrieve all the records from second table

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Select all the record with score greater than or equal to 10.
+-- The records will be ordered by score in descending order.
+SELECT `score`, `name` FROM `second_table` WHERE score >= 10 ORDER BY score DESC;
