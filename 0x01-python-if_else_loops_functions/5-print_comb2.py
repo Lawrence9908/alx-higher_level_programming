c022322f2d0759a4a69df06f3412b438349e94cf
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-12-14 12:16:47 +0200

    new tasks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-for number in range(0, 99):
-    if  number < 9:
-        print("0{}, ".format(number))
-    elif number > 9:
-        print("{0}, ".format(number))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def islower(c):
-    #ord() function finds the unicode of an alphabet
-    if ord(c) in range(97, 123):
+    """Check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
