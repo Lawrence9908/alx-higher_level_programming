1fa8816bbea37d0c57ab6a83e78402e4ad523eca
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-12-14 12:01:16 +0200

    task 0 1 2 3 4 update

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
-
-for number in range(0, 98):
-    if number <= 9:
-        print("{0} = 0x{1}".format(number, number))
-    elif number == 10:
-        print("{0} = 0x{1}".format(number, "a"))
-    elif number == 11:
-        print("{0} = 0X{1}".format(number, "b"))
-    elif number == 12:
-        print("{0} = 0x{1}".format(number, "c"))
-    elif number == 13:
-        print("{0} = 0x{}".format(number, "d"))
-    elif number == 14:
-        print("{0} = 0x{1}".format(number. "e"))
-    elif number == 15:
-        print("{0} = 0x{1}".format(number, "f"))
-    elif number >= 16:
-        print("{0} = 0x{}".format(number, (number - 6)))
+"""Print numbers 0 to 98 in decimal and hexadecimal."""
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
