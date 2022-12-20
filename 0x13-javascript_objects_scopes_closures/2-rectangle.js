1b9393f48cf6d81acab1faae01b60881d1ab37c5
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-12-20 16:37:40 +0200

    javaScript object and scope task

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-class Rectange{
+
+class Rectangle{
     constructor(w, h){
         if(w > 0 && h > 0){
-            this.weight = w;
+            this.width = w;
             this.height = h;
         }
     }
 };
+module.exports = Ractangle;
