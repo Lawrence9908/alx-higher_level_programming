90c2a27b5451b270f9a5eb1601629bca03816aaa
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-12-21 17:48:59 +0200

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,13 @@
 const Rectangle = require("./4-rectangle")
 class Square extends Rectangle{
     charPrint(c){
-        if (c == undefined){
+        if (c != undefined){
+            for (let col = 0; col <= this.height; col++){
+                for (let row = 0; row <= this.width; row++){
+                    console.log("X");
+                }
+            }
+        }else{
             this.print();
         }
     }
