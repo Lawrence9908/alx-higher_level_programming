05a4657210feca8952472421b7c92962d6c10ce4
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-12-21 18:29:52 +0200

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let line = 0; line < list.length; line++) {
   console.log(line);
 }
